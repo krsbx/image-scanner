cd8bc9805054322df34b0f47349abf95d749f522
krsbx <firdaus.sati51@gmail.cm>
2023-02-17 23:03:23 +0700

    chore: rename app name

--- src/components/Overlay/CameraControl.tsx
+++ src/components/Overlay/CameraControl.tsx
@@ -1,16 +1,8 @@
 import React from 'react';
-import { DIMENSSIONS } from '../../utils/constant';
 import PhoneLayout from '../Layout/Phone';
-import { TabletBaseLayout, TabletLargeLayout } from '../Layout/Tablet';
 
 const CameraControl: React.FC<Props> = ({ capture }) => {
-  const { IS_PHONE, IS_LARGE_TABLET } = DIMENSSIONS;
-
-  if (IS_PHONE) return <PhoneLayout capture={capture} />;
-
-  if (IS_LARGE_TABLET) return <TabletLargeLayout capture={capture} />;
-
-  return <TabletBaseLayout capture={capture} />;
+  return <PhoneLayout capture={capture} />;
 };
 type Props = {
   capture: () => void;
